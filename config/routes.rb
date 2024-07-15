ac7ffa296e8f2c2761d5357af2b7e92b13929bcd
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-15 16:15:56 +0000

    added the missing except: parameter within routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :follow_requests, except: [:index, :show, :new, :edit]
   resources :likes, only: [:create, :destroy]
-  resources :photos [:index]
+  resources :photos, except: [:index]
 
   get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
